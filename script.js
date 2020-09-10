d9f487562d17f040146595884e3fb9b2e94dd195
susantikcm <susantikcm@yahoo.com>
2020-09-10 16:34:09 +1000

    Update feedback from Robin

--- script.js
+++ script.js
@@ -1,59 +1,53 @@
-let toggleCheck = (id) => {
+const toggleCheck = (id) => {
     document.querySelector('#' + id).classList.toggle('checked');
 }
 
 const removeItem = (id) => {   
-    let item = document.getElementById(id);
+    const item = document.querySelector('#' + id);
     item.remove();
 }
 
-let createTodo = (todo) => {
-    let countItems = "todo" + (todoList.getElementsByTagName("li").length + 1);
-    let item = document.createElement("li");
-    item.setAttribute("id", countItems);
+const createTodo = () => {
+    const inputTodo = document.querySelector("#newTodo").value;
 
-    let itemCheckbox = document.createElement("input");
-    itemCheckbox.type = "checkbox";
-    itemCheckbox.addEventListener('click', () => toggleCheck(item.id));
-
-    let itemLabel = document.createElement("label");
-    itemLabel.appendChild(document.createTextNode(todo)); 
-
-    let itemDeleteButton = document.createElement("button");
-    itemDeleteButton.textContent = "Delete";
-    // itemDeleteButton.className = "delete";
-    itemDeleteButton.addEventListener('click', () => removeItem(item.id));
-
-    item.appendChild(itemCheckbox);
-    item.appendChild(itemLabel);
-    item.appendChild(itemDeleteButton);
- 
-    return item;
-}
-
-let addNewTodo = () => {
-    let inputTodo = document.querySelector("#newTodo");
-
-    if(inputTodo.value === "") {
+    if(inputTodo === "") {
         return;
     }
     else {
-        let newItem = createTodo(inputTodo.value);
-        let todoList = document.querySelector("#todoList");
-        todoList.appendChild(newItem);
-        inputTodo.value = "";
+        const countItems = "todo" + (todoList.getElementsByTagName("li").length + 1);
+        const item = document.createElement("li");
+        item.setAttribute("id", countItems);
+
+        const itemCheckbox = document.createElement("input");
+        itemCheckbox.type = "checkbox";
+        itemCheckbox.addEventListener('click', () => toggleCheck(item.id));
+
+        const itemLabel = document.createElement("label");
+        itemLabel.appendChild(document.createTextNode(inputTodo)); 
+
+        const itemDeconsteButton = document.createElement("button");
+        itemDeconsteButton.textContent = "Delete";
+        // itemDeconsteButton.className = "deconste";
+        itemDeconsteButton.addEventListener('click', () => removeItem(item.id));
+
+        item.appendChild(itemCheckbox);
+        item.appendChild(itemLabel);
+        item.appendChild(itemDeconsteButton);
+    
+        document.querySelector("#todoList").appendChild(item);
+        document.querySelector("#newTodo").value = "";
     }
 }
 
 document.querySelector("#add").addEventListener('click', () => {
     event.preventDefault();  
-    addNewTodo();
+    createTodo();
 });
 
 document.querySelector("#newTodo").addEventListener('keyup', (event) => {
     if(event.keyCode === 13 || event.key === "Enter")
     {
         event.preventDefault();
-        addNewTodo();
+        createTodo();
     }
 });
